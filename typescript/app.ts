f7edebf2b1315700717dd0e1ed9d54196a75c395
yankasantos <yankasantos0107@gmail.com>
2020-11-08 17:55:56 -0300

    aprendendo um pouco mais de typescript

---
+++ typescript/app.ts
@@ -0,0 +1,9 @@
+let message: string = "oi estou no compilador";
+console.log(message);
+
+let episode: number = 4
+console.log("episodio" + episode)
+
+let serie: string
+serie = "Jessica Jones"
+console.log("minha serie preferida é "+ serie)
---
+++ typescript/dist/app.js
@@ -0,0 +1,7 @@
+var message = "oi estou no compilador";
+console.log(message);
+var episode = 4;
+console.log("episodio" + episode);
+var serie;
+serie = "Jessica Jones";
+console.log("minha serie preferida é " + serie);
